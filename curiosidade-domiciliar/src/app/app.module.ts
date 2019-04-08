9de0eead7f017058aa25cdd60088a5863a9a49cb
gradzio <gradzio@bettercollective.com>
2019-04-08 11:43:12 +0200

    Added prod build to ci/cd

--- curiosidade-domiciliar/src/app/app.module.ts
+++ curiosidade-domiciliar/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
